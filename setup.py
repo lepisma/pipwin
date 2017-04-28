ee29c8c0cc109dd454621095aa61c061a13eeaa3
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-04-28 02:37:32 -0400

    v0.3.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setup(
     name="pipwin",
-    version="0.3",
+    version="0.3.1",
     description="pipwin installs compiled python binaries on windows provided by Christoph Gohlke",
     long_description=readme,
     author="lepisma",
